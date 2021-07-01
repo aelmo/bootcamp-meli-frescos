2f9a31c4631281d7fd01fefc328c280a92cfc666
Amedeo Elmo <amedeo.elmo@mercadolivre.com>
2021-07-01 10:31:06 -0300

    Spring Scaffolding

---
+++ src/main/java/com/mercadolibre/bootcampmelifrescos/BootcampMeliFrescosApplication.java
@@ -0,0 +1,13 @@
+package com.mercadolibre.bootcampmelifrescos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootcampMeliFrescosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootcampMeliFrescosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mercadolibre/bootcampmelifrescos/BootcampMeliFrescosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mercadolibre.bootcampmelifrescos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootcampMeliFrescosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
