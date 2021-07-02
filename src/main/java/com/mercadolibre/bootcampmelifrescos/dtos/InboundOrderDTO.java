ce139a0388f6c27caf8c0225cc980ff14d83c4b5
emanueli silva <emanueli.silva@mercadolivre.com>
2021-07-02 11:15:20 -0300

    refactoring variables type

--- src/main/java/com/mercadolibre/bootcampmelifrescos/dtos/InboundOrderDTO.java
+++ src/main/java/com/mercadolibre/bootcampmelifrescos/dtos/InboundOrderDTO.java
@@ -1,16 +1,22 @@
 package com.mercadolibre.bootcampmelifrescos.dtos;
 
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 import java.util.List;
 
+@Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class InboundOrderDTO {
-    private int orderNumber;
+    private Long orderNumber;
     private LocalDate orderDate;
     private SectionDTO section;
     private List<BatchDTO> batchStock;
+
+    public Long getSectionCode(){
+        return section.getSectionCode();
+    }
 }
--- src/main/java/com/mercadolibre/bootcampmelifrescos/dtos/request/InboundOrderRequest.java
+++ src/main/java/com/mercadolibre/bootcampmelifrescos/dtos/request/InboundOrderRequest.java
@@ -1,11 +1,30 @@
 package com.mercadolibre.bootcampmelifrescos.dtos.request;
 
+import com.mercadolibre.bootcampmelifrescos.dtos.BatchDTO;
 import com.mercadolibre.bootcampmelifrescos.dtos.InboundOrderDTO;
+import com.mercadolibre.bootcampmelifrescos.dtos.SectionDTO;
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDate;
+import java.util.List;
+
+@Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class InboundOrderRequest {
     private InboundOrderDTO inboundOrder;
+
+    public List<BatchDTO> getBatchDTOList(){
+        return inboundOrder.getBatchStock();
+    }
+
+    public LocalDate getOrderDate(){
+        return inboundOrder.getOrderDate();
+    }
+
+    public SectionDTO getSectionOrder(){
+        return inboundOrder.getSection();
+    }
 }
