b92851831428165e0c96304c0ecddd87aab0e7ba
Jairo Junior <jairo.ajunior@mercadolivre.com>
2021-07-05 16:40:00 -0300

    Resolving comments

--- src/main/java/com/mercadolibre/bootcampmelifrescos/dtos/CartProductsDTO.java
+++ src/main/java/com/mercadolibre/bootcampmelifrescos/dtos/CartProductsDTO.java
@@ -4,10 +4,18 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class CartProductsDTO {
+    @NotBlank(message = "the product id cannot be blank")
     private Long productId;
+
+    @Positive(message = "The quantity cannot be negative")
+    @NotBlank(message = "The quantity cannot be null")
     private Integer quantity;
 }
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -30,14 +30,25 @@ VALUES (1, "2021-07-01", 1, 2),
        (2, "2021-06-30", 2, 3),
        (3, "2021-06-29", 3, 1);
 
-INSERT INTO product (id, name, seller_id, category_id)
-VALUES (1, "Banana", 2, 1),
-       (2, "Apple", 3, 2),
-       (3, "Potato", 1, 3);
+INSERT INTO product (id, name, seller_id, category_id, amount)
+VALUES (1, "Banana", 2, 1, 10),
+       (2, "Apple", 3, 2, 10),
+       (3, "Potato", 1, 3, 10);
 
 INSERT INTO batch (id,current_temperature,minimum_temperature,initial_quantity,current_quantity,manufacturing_date,manufacturing_time,due_date,order_id, product_id)
 VALUES (1,18.0,8.5,10,8,"2021-06-27","2021-06-27 5:00:00","2021-06-30",2, 1),
        (2,18.0,8.5,10,8,"2021-06-27","2021-06-27 1:00:00","2021-06-30",2, 1),
        (3,20.2,10.2,10,8,"2021-06-25","2021-06-25 16:00:00","2021-06-30",2, 1);
+
+INSERT INTO purchase_statuses (id, title)
+VALUES (1,'Paid');
+
+INSERT INTO purchase_statuses (id, title)
+VALUES (2,'In Process');
+
+INSERT INTO purchase_statuses (id, title)
+VALUES (3,'Refused');
+
+
 
 
