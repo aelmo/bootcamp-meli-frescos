b679a48cd883f9be04017da3e32e2d6edc28d73a
Jairo Junior <jairo.ajunior@mercadolivre.com>
2021-07-05 16:38:23 -0300

    Adding services

--- src/main/java/com/mercadolibre/bootcampmelifrescos/service/PurchaseOrderService.java
+++ src/main/java/com/mercadolibre/bootcampmelifrescos/service/PurchaseOrderService.java
@@ -1,4 +1,8 @@
 package com.mercadolibre.bootcampmelifrescos.service;
 
+import com.mercadolibre.bootcampmelifrescos.dtos.request.PurchaseOrderDTO;
+import com.mercadolibre.bootcampmelifrescos.dtos.response.PurchaseAmountDTO;
+
 public interface PurchaseOrderService {
+    PurchaseAmountDTO createPurchaseOrder(PurchaseOrderDTO purchaseOrderDTO) throws Exception;
 }
