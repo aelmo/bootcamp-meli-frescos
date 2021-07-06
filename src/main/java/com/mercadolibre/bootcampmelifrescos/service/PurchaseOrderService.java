fc46fa2800c75d8da1a67f458e7fc33cae5cbcec
Jairo Junior <jairo.ajunior@mercadolivre.com>
2021-07-06 14:39:57 -0300

    Changing the method of inserting a new purchase order in database

--- src/main/java/com/mercadolibre/bootcampmelifrescos/service/PurchaseOrderService.java
+++ src/main/java/com/mercadolibre/bootcampmelifrescos/service/PurchaseOrderService.java
@@ -2,7 +2,9 @@ package com.mercadolibre.bootcampmelifrescos.service;
 
 import com.mercadolibre.bootcampmelifrescos.dtos.request.PurchaseOrderDTO;
 import com.mercadolibre.bootcampmelifrescos.dtos.response.PurchaseAmountDTO;
+import com.mercadolibre.bootcampmelifrescos.model.PurchaseOrder;
 
 public interface PurchaseOrderService {
-    PurchaseAmountDTO createPurchaseOrder(PurchaseOrderDTO purchaseOrderDTO) throws Exception;
+    PurchaseOrder createPurchaseOrder(PurchaseOrderDTO purchaseOrderDTO) throws Exception;
+    PurchaseAmountDTO getAmountOfAnPurchaseOrder(PurchaseOrder purchaseOrder);
 }
