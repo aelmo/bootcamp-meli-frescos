c5d3da13c71a48164b5e48590799e1b124ec43c5
Jairo Junior <jairo.ajunior@mercadolivre.com>
2021-07-06 14:39:57 -0300

    Changing the method of inserting a new purchase order in database

--- src/main/java/com/mercadolibre/bootcampmelifrescos/service/impl/PurchaseOrderServiceImpl.java
+++ src/main/java/com/mercadolibre/bootcampmelifrescos/service/impl/PurchaseOrderServiceImpl.java
@@ -3,62 +3,69 @@ package com.mercadolibre.bootcampmelifrescos.service.impl;
 import com.mercadolibre.bootcampmelifrescos.dtos.request.PurchaseOrderDTO;
 import com.mercadolibre.bootcampmelifrescos.dtos.request.PurchaseRequestProductsDTO;
 import com.mercadolibre.bootcampmelifrescos.dtos.response.PurchaseAmountDTO;
-import com.mercadolibre.bootcampmelifrescos.model.Batch;
-import com.mercadolibre.bootcampmelifrescos.model.Cart;
-import com.mercadolibre.bootcampmelifrescos.model.Product;
-import com.mercadolibre.bootcampmelifrescos.model.PurchaseOrder;
+import com.mercadolibre.bootcampmelifrescos.model.*;
 import com.mercadolibre.bootcampmelifrescos.repository.*;
 import com.mercadolibre.bootcampmelifrescos.service.PurchaseOrderService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 @Service
 @AllArgsConstructor
 public class PurchaseOrderServiceImpl implements PurchaseOrderService {
     private final PurchaseOrderRepository purchaseOrderRepository;
-    private final CartRepository cartRepository;
     private final PurchaseStatusesRepository purchaseStatusesRepository;
     private final ProductRepository productRepository;
     private final BatchRepository batchRepository;
     private final BuyerRepository buyerRepository;
+    private final PurchaseProductsRepository purchaseProductsRepository;
 
 
     @Override
-    public PurchaseAmountDTO createPurchaseOrder(PurchaseOrderDTO purchaseOrderDTO) throws Exception{
+    public PurchaseOrder createPurchaseOrder(PurchaseOrderDTO purchaseOrderDTO) throws Exception{
         PurchaseOrder purchaseOrderToSave = new PurchaseOrder();
         Float total = 0.0F;
 
         Set<Product> products = new HashSet<>();
 
-        for(PurchaseRequestProductsDTO product: purchaseOrderDTO.getProducts()) {
-            Long productId = product.getProductId();
-            Integer productQuantity = product.getQuantity();
-
-            Float productPrice = productRepository.getOne(productId).getAmount();
-            total = total + (productPrice * productQuantity);
-
-            products.add(productRepository.getOne(productId));
-            removeProductFromBatch(productId, productQuantity);
-        }
-
-        purchaseOrderToSave.setCart(saveCart(products));
+        purchaseOrderToSave = purchaseOrderRepository.save(purchaseOrderToSave);
+        purchaseOrderToSave.setPurchaseOrderProducts(createPurchaseOrderProductsSet(purchaseOrderToSave, purchaseOrderDTO.getProducts()));
         purchaseOrderToSave.setDate(purchaseOrderDTO.getDate());
         purchaseOrderToSave.setStatus(purchaseStatusesRepository.getOne(purchaseOrderDTO.getOrderStatus().getStatusCode()));
         purchaseOrderToSave.setBuyer(buyerRepository.getOne(purchaseOrderDTO.getBuyerId()));
 
-        purchaseOrderRepository.save(purchaseOrderToSave);
-
-        return new PurchaseAmountDTO(Double.valueOf(total));
+        return purchaseOrderRepository.save(purchaseOrderToSave);
     }
 
+    @Override
+    public PurchaseAmountDTO getAmountOfAnPurchaseOrder(PurchaseOrder purchaseOrder) {
+        List<PurchaseOrderProducts> purchaseOrderProducts =  purchaseProductsRepository.findAllBypurchaseOrder(purchaseOrder);
+        Double totalAmount = 0.0;
 
-    public Cart saveCart(Set<Product> products) {
-        Cart cart = new Cart();
-        cart.setProducts(products);
-        return cartRepository.save(cart);
+        for (PurchaseOrderProducts purchaseOrderProduct: purchaseOrderProducts) {
+            totalAmount = totalAmount + purchaseOrderProduct.getProduct().getAmount() * purchaseOrderProduct.getQuantity();
+        }
+
+        return new PurchaseAmountDTO(totalAmount);
+    }
+
+    public Set<PurchaseOrderProducts> createPurchaseOrderProductsSet(PurchaseOrder purchaseOrder, Set<PurchaseRequestProductsDTO> requestProductsDTO) throws Exception {
+        Set<PurchaseOrderProducts> purchaseOrderProductsSet = new HashSet<>();
+
+        for(PurchaseRequestProductsDTO product: requestProductsDTO) {
+            removeProductFromBatch(product.getProductId(), product.getQuantity());
+
+            PurchaseOrderProducts purchaseOrderProducts = new PurchaseOrderProducts();
+            purchaseOrderProducts.setPurchaseOrder(purchaseOrder);
+            purchaseOrderProducts.setProduct(productRepository.getOne(product.getProductId()));
+            purchaseOrderProducts.setQuantity(product.getQuantity());
+            purchaseOrderProductsSet.add(purchaseProductsRepository.save(purchaseOrderProducts));
+        }
+
+        return purchaseOrderProductsSet;
     }
 
     public void removeProductFromBatch(Long productId, Integer quantity) throws Exception {
