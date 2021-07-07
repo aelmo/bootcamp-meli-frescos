038e091eb7c92c6b9af1f65656da847aed0309e9
emanueli silva <emanueli.silva@mercadolivre.com>
2021-07-07 18:19:47 -0300

    Adding endpoint controller to list quantity

---
+++ src/main/java/com/mercadolibre/bootcampmelifrescos/controller/WarehouseBatchController.java
@@ -0,0 +1,24 @@
+package com.mercadolibre.bootcampmelifrescos.controller;
+
+
+import com.mercadolibre.bootcampmelifrescos.dtos.response.WarehouseBatchResponse;
+import com.mercadolibre.bootcampmelifrescos.service.WarehouseBatchService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/fresh-products/warehouse")
+@AllArgsConstructor
+public class WarehouseBatchController {
+    private final WarehouseBatchService warehouseBatchService;
+
+    @GetMapping("/{productId}")
+    public ResponseEntity<WarehouseBatchResponse> getWarehouseBatchQuantityByProduct(@PathVariable("productId") Long productId) throws Exception {
+        return new ResponseEntity<>(warehouseBatchService.getWarehouseBatchQuantityByProduct(productId), HttpStatus.OK);
+    }
+}
