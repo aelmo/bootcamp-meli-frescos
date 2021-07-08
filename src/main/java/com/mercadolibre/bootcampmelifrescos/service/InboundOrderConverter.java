c0167c58194a624f82d1103d5ba275afef1a7681
emanueli silva <emanueli.silva@mercadolivre.com>
2021-07-08 18:33:57 -0300

    validating section/warehouse values us01

--- src/main/java/com/mercadolibre/bootcampmelifrescos/service/InboundOrderConverter.java
+++ src/main/java/com/mercadolibre/bootcampmelifrescos/service/InboundOrderConverter.java
@@ -1,10 +1,10 @@
 package com.mercadolibre.bootcampmelifrescos.service;
 
 import com.mercadolibre.bootcampmelifrescos.dtos.InboundOrderDTO;
-import com.mercadolibre.bootcampmelifrescos.exceptions.api.NotFoundApiException;
+import com.mercadolibre.bootcampmelifrescos.exceptions.api.ApiException;
 import com.mercadolibre.bootcampmelifrescos.model.InboundOrder;
 
 public interface InboundOrderConverter {
 
-    InboundOrder dtoToEntity(InboundOrderDTO inboundOrderDTO) throws NotFoundApiException;
+    InboundOrder dtoToEntity(InboundOrderDTO inboundOrderDTO) throws ApiException;
 }
