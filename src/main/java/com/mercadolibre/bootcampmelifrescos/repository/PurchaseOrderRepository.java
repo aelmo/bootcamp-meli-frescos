8b42fd0e29c6e8a5dbc774db5af4900a86346145
Jairo Junior <jairo.ajunior@mercadolivre.com>
2021-07-12 08:24:02 -0300

    Adding the necessary queries for requisite 6

--- src/main/java/com/mercadolibre/bootcampmelifrescos/repository/PurchaseOrderRepository.java
+++ src/main/java/com/mercadolibre/bootcampmelifrescos/repository/PurchaseOrderRepository.java
@@ -1,5 +1,6 @@
 package com.mercadolibre.bootcampmelifrescos.repository;
 
+import com.mercadolibre.bootcampmelifrescos.dtos.request.PurchaseOrderDTO;
 import com.mercadolibre.bootcampmelifrescos.model.PurchaseOrder;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
