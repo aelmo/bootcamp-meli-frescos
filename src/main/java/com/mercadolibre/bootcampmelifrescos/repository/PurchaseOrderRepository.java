23fe7fcdd919f659ff833420708167b11a6abc00
Jairo Junior <jairo.ajunior@mercadolivre.com>
2021-07-12 08:24:02 -0300

    Adding the necessary queries for requisite 6

--- src/main/java/com/mercadolibre/bootcampmelifrescos/repository/PurchaseOrderRepository.java
+++ src/main/java/com/mercadolibre/bootcampmelifrescos/repository/PurchaseOrderRepository.java
@@ -1,7 +1,15 @@
 package com.mercadolibre.bootcampmelifrescos.repository;
 
+import com.mercadolibre.bootcampmelifrescos.dtos.request.PurchaseOrderDTO;
 import com.mercadolibre.bootcampmelifrescos.model.PurchaseOrder;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.time.LocalDate;
+import java.util.List;
 
 public interface PurchaseOrderRepository extends JpaRepository<PurchaseOrder,Long> {
+    @Query(value = "select id from purchase_order where purchase_order.buyer_id != \"NULL\" AND purchase_order.date > :startDate AND purchase_order.date < :endDate", nativeQuery = true)
+    List<Integer> getAllPurchaseOrderByPeriod(@Param("startDate") LocalDate startDate, @Param("endDate") LocalDate endDate);
 }
--- src/main/java/com/mercadolibre/bootcampmelifrescos/repository/PurchaseProductsRepository.java
+++ src/main/java/com/mercadolibre/bootcampmelifrescos/repository/PurchaseProductsRepository.java
@@ -1,12 +1,30 @@
 package com.mercadolibre.bootcampmelifrescos.repository;
 
+import com.mercadolibre.bootcampmelifrescos.dtos.MostSoldProductAndQuantityDTO;
 import com.mercadolibre.bootcampmelifrescos.model.PurchaseOrder;
 import com.mercadolibre.bootcampmelifrescos.model.PurchaseOrderProducts;
+import org.apache.tomcat.jni.Local;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.time.LocalDate;
 import java.util.List;
+import java.util.Map;
 
 public interface PurchaseProductsRepository extends JpaRepository<PurchaseOrderProducts, Long> {
 
     List<PurchaseOrderProducts> findAllByPurchaseOrder(PurchaseOrder purchaseOrder);
+
+    @Query(value = "SELECT purchase_products.product_id as productId, SUM(purchase_products.quantity) as quantity\n" +
+            "from purchase_products\n" +
+            "INNER JOIN purchase_order\n" +
+            "ON purchase_products.purchase_order_id = purchase_order.id\n" +
+            "WHERE purchase_order.date > :startDate\n" +
+            "AND purchase_order.date < :endDate\n" +
+            "GROUP BY purchase_products.product_id\n" +
+            "ORDER BY quantity\n" +
+            "DESC LIMIT 1;", nativeQuery = true)
+    Map<String, Integer> getMostSoldProductIdAndQuantity(LocalDate startDate, LocalDate endDate);
+
 }
